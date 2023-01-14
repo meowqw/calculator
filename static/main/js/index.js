af28becdc91e124e0c13ad38f8e65ca210a2e5c7
meatqw <lexerdxx@gmail.com>
2023-01-14 13:13:45 +0700

    static

--- static/main/js/index.js
+++ static/main/js/index.js
@@ -5,25 +5,25 @@ new Vue({
 
         // API endpoint
         diameters: [
-            { 'value': '20 см', 'weight': 10 },
-            { 'value': '30 см', 'weight': 20 }
+            { 'value': '20 см', 'price': 20, 'total': 0 },
+            { 'value': '30 см', 'price': 30, 'total': 0 }
         ],
 
         wallThickness: [],
 
         wallMaterial: [
-            { 'value': 'Газоблок', 'weight': 10 },
-            { 'value': 'Кирпич', 'weight': 20 },
-            { 'value': 'Бетон', 'weight': 30 }
+            { 'value': 'Газоблок', 'price': 10, 'total': 0 },
+            { 'value': 'Кирпич', 'price': 20, 'total': 0 },
+            { 'value': 'Бетон', 'price': 30, 'total': 0 }
         ],
 
         coefficients: [
-            { 'value': 'Глубина отверстий более 80 см', 'weight': 20 },
-            { 'value': 'Глубина отверстий менее 80 см', 'weight': 10 },
+            { 'value': 'Глубина отверстий более 80 см', 'price': 10, 'total': 0 },
+            { 'value': 'Глубина отверстий менее 80 см', 'price': 20, 'total': 0 },
         ],
         //
 
-        
+
         items: [],
         count: 0,
 
@@ -35,40 +35,130 @@ new Vue({
             'coefficient': { "value": 0, 'total': 0 },
             'remoteness': { "value": 0, 'total': 0 },
             'total': 0
-        }
+        },
         //
 
     },
     methods: {
-        addNewItem: function() {
+        addNewItem: async function () {
             var item = {
-                'diameters': { "value": 0, 'total': 0 },
-                'thickness': { "value": 0, 'total': 0 },
-                'material': { "value": 0, 'total': 0 },
-                'coefficient': { "value": 0, 'total': 0 },
-                'remoteness': { "value": 0, 'total': 0 },
-                'total': 0
+                'diameters': { "value": 'Не выбрано', 'price': 0, 'total': 0 },
+                'thickness': { "value": 0, 'price': 0, 'total': 0 },
+                'material': { "value": 'Не выбрано', 'price': 0, 'total': 0 },
+                'coefficient': { "value": 'Не выбрано', 'price': 0, 'total': 0 },
+                'total': 0,
+                'count': 1,
             }
-            this.items.push({'id': this.count, 'result': item});
+            this.items.push({ 'id': this.count, 'result': item });
+
             this.count++;
 
         },
-        delItem: function(id){
+        delItem: async function (id) {
+            for (var i in this.items) {
+                if (this.items[i].id == id) {
+                    this.items.splice(i, 1);
+                }
+            }
+        },
+        calculate: function () {
 
         },
-        calculate: function(){
 
-        }
+        openItem: function (id) {
+            buttons = document.getElementsByClassName('btn-reset accordion hero__accordion')
+            panels = document.getElementsByClassName('panel hero__panel')
+
+            for (var i in buttons) {
+                if (buttons[i].id == id) {
+                    if (buttons[i].classList.toString().includes('active')) {
+                        buttons[i].classList.remove('active')
+                    } else {
+                        buttons[i].classList.add('active')
+                    }
+                }
+            }
+            for (var i in panels) {
+                if (panels[i].id == id) {
+                    if (panels[i].style.cssText == 'max-height: 716px;') {
+                        panels[i].style.cssText = ''
+                    } else {
+                        panels[i].style.cssText += 'max-height: 716px;'
+                    }
+                }
+            }
+        },
+        changeValue: function (id, value, item) {
+            for (var i in this.items) {
+                if (this.items[i].id == id) {
+                    this.items[i].result[value] = item
+                }
+            }
+            this.caclulateTotalItem(id)
+        },
+
+        slider: function (el, id) {
+            if (el == 'slider') {
+                document.getElementById('amount' + id).value = document.getElementById('slider' + id).value
+            } else if (el == 'amount') {
+                document.getElementById('slider' + id).value = document.getElementById('amount' + id).value
+            }
+            var sliderValue = document.getElementById('amount' + id).value
+            this.changeValue(id, 'thickness', { 'value': sliderValue, 'price': sliderValue, 'total': sliderValue })
+        },
+
+        changeCount: function (id) {
+            var count = document.getElementById('count' + id).value
+            for (var i in this.items) {
+                if (this.items[i].id == id) {
+                    this.items[i].result['count'] = count
+                }
+            }
+
+            this.caclulateTotalItem(id);
+        },
+
+        caclulateTotalItem: function (id) {
+            for (var i in this.items) {
+                if (this.items[i].id == id) {
+
+                    this.items[i].result['diameters'] = {
+                        'value': this.items[i].result['diameters'].value,
+                        'total': Number(this.items[i].result['diameters'].price),
+                        'price': Number(this.items[i].result['diameters'].price),
+                    }
+                    this.items[i].result['thickness'] = {
+                        'value': this.items[i].result['thickness'].value,
+                        'price': Number(this.items[i].result['thickness'].price),
+                        'total': Number(this.items[i].result['thickness'].price * this.items[i].result['diameters'].price)
+                    }
+                    this.items[i].result['material'] = {
+                        'value': this.items[i].result['material'].value,
+                        'price': Number(this.items[i].result['material'].price),
+                        'total': Number(this.items[i].result['material'].price * this.items[i].result['diameters'].total)
+                    }
+                    this.items[i].result['coefficient'] = {
+                        'value': this.items[i].result['coefficient'].value,
+                        'price': Number(this.items[i].result['coefficient'].price),
+                        'total': Number(this.items[i].result['coefficient'].total)
+                    }
+
+                    var totals = [this.items[i].result['diameters'].total,
+                    this.items[i].result['material'].total, 
+                    this.items[i].result['thickness'].total,
+                    this.items[i].result['coefficient'].total]
+    
+                    let result = totals.reduce(function(sum, elem) {
+                        return sum + elem;
+                    }, 0);
+                    this.items[i].result.total = result * this.items[i].result.count
+                }
+            }
+        },
 
     },
-    mounted() {
+    async mounted() {
+        // this.renderScript();
 
-        let script = document.createElement("script");
-        script.setAttribute(
-        "src",
-        "/static/main/js/main.js"
-        );
-        document.head.appendChild(script);
-        //
     }
 })
--- static/main/js/main.js
+++ static/main/js/main.js
@@ -183,6 +183,7 @@ var rangeBullet = document.getElementById("rs-bullet");
 rangeSlider?.addEventListener("input", showSliderValue, false);
 function showSliderValue() {
   rangeBullet.innerHTML = rangeSlider.value;
+  
   var bulletPosition = rangeSlider.value / rangeSlider.max;
   rangeBullet.style.left = bulletPosition * 578 + "px";
 }
